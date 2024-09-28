99ae23595a4bdf2bf40c6dc729d7946a59ba6aff
Afaiz8720 <96206044+Afaiz8720@users.noreply.github.com>
2024-09-28 12:21:28 +0530

    u

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = process.env.BACKEND_URL + "api/v1/user";
-export const JOB_API_END_POINT = process.env.BACKEND_URL + "api/v1/job";
-export const APPLICATION_API_END_POINT = process.env.BACKEND_URL + "api/v1/application";
-export const COMPANY_API_END_POINT = process.env.BACKEND_URL + "api/v1/company";
+export const USER_API_END_POINT = "https://job-hunt123-git-main-faiz-ahmads-projects-da10913f.vercel.app/api/v1/user";
+export const JOB_API_END_POINT = "https://job-hunt123-git-main-faiz-ahmads-projects-da10913f.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://job-hunt123-git-main-faiz-ahmads-projects-da10913f.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT = "https://job-hunt123-git-main-faiz-ahmads-projects-da10913f.vercel.app/api/v1/company";
